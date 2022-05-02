f01ac5ca5e318b50f2f4a8dd827e29961fa60f2e
PierreEdFils <epierrefils@gmail.com>
2022-05-02 17:04:14 -0400

    Initial commit

---
+++ pythonsyntax.py
@@ -0,0 +1,26 @@
+
+#1
+#print ("Water; there is not a drop of water there! Were Niagara but a cataract of sand, would you travel your thousand miles to see it?")
+
+#2
+#print("Hello World!")
+#print("Deep into distant woodlands winds a mazy way, reaching to overlapping spurs of mountains bathed in their hill-side blue.")
+
+#3
+ #print ("Hello " +"Pierre")
+
+#4
+print("How do you make a hot dog stand?")
+print("You take away its chair!")
+
+#4 Variables
+todays_date = "4/26/2022"
+
+
+#5 Arithmetic
+price =10
+oranges = 12
+product = price  * oranges
+
+product =1398
+remainder = product % 11
